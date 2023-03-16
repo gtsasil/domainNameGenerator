c4befac27cd62e86864202bab4e1890ad631f314
Gleisson Santos Silva <gtsasil@gmail.com>
2023-03-16 14:25:17 +0000

    End of Domain Name Genarator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function() {};
+document.querySelector("#btn").addEventListener("click", () => {
+  domainNameGenerator().forEach((element, index) => {
+    const elem = (sel, par) => (par || document).querySelector(sel);
+    const newElem = (tag, prop) =>
+      Object.assign(document.createElement(tag), prop);
+    const elemParent = elem("#newDomain");
+    elemParent.append(
+      newElem("p", { textContent: index + 1 + " -  " + element })
+    );
+  });
+});
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let result = [];
+
+let domainNameGenerator = () => {
+  let pronoun = ["A", "The", "Our"];
+  let adjective = ["great", "amazing", "wild", "big", "rabbit"];
+  let noun = ["jogger", "cat", "services", "car", "keys"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adjective[j] + noun[k] + ".com");
+        result.push(pronoun[i] + adjective[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return result;
 };
